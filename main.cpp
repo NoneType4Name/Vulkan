f437bbfd6e3490bb96d8bf103eb9142e1890fe31
NoneType4Name <90710372+NoneType4Name@users.noreply.github.com>
2023-06-04 22:43:51 +0300

    Added DebugExt.

--- main.cpp
+++ main.cpp
@@ -7,22 +7,24 @@
 #include <cstring>
 #include <vector>
 #include <format>
+#include <string>
 
 // #include <P7_Client.h>
 using namespace std;
 
-class HelloTriangleApplication
+class App
 {
 public:
     const uint16_t WIDTH{1000};
     const uint16_t HEIGHT{1000};
     const vector<const char *> ValidationLayers = {
         "VK_LAYER_KHRONOS_validation"};
-#ifdef NDEBUG
-    const bool EnableValidationLayers = false;
-#else
-    const bool EnableValidationLayers = true;
-#endif
+
+    #ifdef NDEBUG
+        const bool EnableValidationLayers = false;
+    #else
+        const bool EnableValidationLayers = true;
+    #endif
 
     void run()
     {
@@ -36,6 +38,16 @@ public:
 private:
     GLFWwindow *window;
     VkInstance instance;
+    VkDebugUtilsMessengerEXT DebugMessenger;
+    VkDebugUtilsMessengerCreateInfoEXT DbgMessengerCreateInfo{
+        VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+        NULL,
+        NULL,
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
+        VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
+        DebugCallback,
+        this
+    };
 
     void initWindow()
     {
@@ -48,6 +60,7 @@ private:
     void initVulkan()
     {
         CreateVKinstance();
+        SetupDebugMessenger();
     }
 
     void mainLoop()
@@ -59,12 +72,14 @@ private:
     }
 
     void cleanup()
-    {
+    {   
+        if (EnableValidationLayers) DestroyDebugUtilsMessengerEXT(instance, DebugMessenger, nullptr);
         vkDestroyInstance(instance, NULL);
         glfwDestroyWindow(window);
         glfwTerminate();
     }
 
+    // Support Functios
     void CreateVKinstance()
     {
         if (EnableValidationLayers && !CheckValidationLayers())
@@ -91,20 +106,27 @@ private:
         VkInstanceCreateInfo CreateInfo{};
         CreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         CreateInfo.pApplicationInfo = &AppInfo;
+
         if (EnableValidationLayers)
         {
             CreateInfo.enabledLayerCount = uint32_t(ValidationLayers.size());
             CreateInfo.ppEnabledLayerNames = ValidationLayers.data();
+            CreateInfo.pNext = &DbgMessengerCreateInfo;
         }
+        else{
+            CreateInfo.enabledLayerCount = 0;
+            CreateInfo.pNext = nullptr;
+        }
+
         vector<const char*> Extensions(glfwExtensions, glfwExtensions + glfwExtensionsCount);
         Extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
         CreateInfo.enabledExtensionCount = static_cast<uint32_t>(Extensions.size());
         CreateInfo.ppEnabledExtensionNames = Extensions.data();
+        
         VkResult InstanceCreateCode = vkCreateInstance(&CreateInfo, nullptr, &instance);
         if (InstanceCreateCode != VK_SUCCESS)
         {   
-            cout << InstanceCreateCode << endl;
-            throw runtime_error(format("{}: {}", "Failed create instance, error code", string_VkResult(InstanceCreateCode)));
+            throw runtime_error(format("Failed create instance, error code: {}.", string_VkResult(InstanceCreateCode)));
         }
     }
 
@@ -133,11 +155,77 @@ private:
         }
         return true;
     }
+
+    void SetupDebugMessenger()
+    {
+        if (!EnableValidationLayers) return;
+        VkResult CreateDebugUtilsMessengerEXT_result{CreateDebugUtilsMessengerEXT(instance, &DbgMessengerCreateInfo, nullptr, &DebugMessenger)};
+        if (CreateDebugUtilsMessengerEXT_result != VK_SUCCESS) throw runtime_error(format("Failed to setup Debug Messenger, error: {}.", string_VkResult(CreateDebugUtilsMessengerEXT_result)));
+    }
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT MessageLevel,
+        VkDebugUtilsMessageTypeFlagsEXT MessageType,
+        const VkDebugUtilsMessengerCallbackDataEXT *CallbackData,
+        void *UserData){
+            string StrMessageType;
+            switch (MessageType)
+            {
+            case VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT:
+                StrMessageType = "GeneralError";
+                break;
+            case VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT:
+                StrMessageType = "SpecificationError";
+                break;
+            case VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT:
+                StrMessageType = "MisuseVulkanApiError";
+                break;
+            
+            default:
+                StrMessageType = string_VkDebugUtilsMessageTypeFlagsEXT(MessageType);
+                break;
+            }
+            // switch (MessageLevel)
+            // {
+            // case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+            //     // debug
+            //     break;
+            // case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+            //     // info
+            //     break;
+            // case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+            //     // warn
+            //     break;
+            // case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+            //     // error
+            //     break;
+            // }
+            cerr << "Validation layer get " << StrMessageType << ": " << CallbackData->pMessage << endl;
+            return VK_FALSE;
+    }
+
+    // VkDebugUtilsMessengerCreateInfoEXT DbgUtilsMessengerCreateInfo(){
+    //     return CreateInfo;
+    // }
+
+    // Ext Functions load.
+
+    VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo, const VkAllocationCallbacks *pAllocator, VkDebugUtilsMessengerEXT *pMessenger)
+    {
+        PFN_vkCreateDebugUtilsMessengerEXT _crtDbgUtMsgrEXT = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+        return (_crtDbgUtMsgrEXT == nullptr ? VK_ERROR_EXTENSION_NOT_PRESENT : _crtDbgUtMsgrEXT(instance, pCreateInfo, pAllocator, pMessenger));
+    }
+
+    void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT messenger, const VkAllocationCallbacks *pAllocator){
+        PFN_vkDestroyDebugUtilsMessengerEXT _dstrDbgUtMsgrEXT = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+        if (_dstrDbgUtMsgrEXT != nullptr) _dstrDbgUtMsgrEXT(instance, messenger, pAllocator);
+    }
+
 };
 
 int main(int argc, char *argv[])
 {
-    HelloTriangleApplication app;
+    App app;
     try
     {
         app.run();
